aca98383ff61d85880ba9c29404b70267077e6bb
Tyler Kandarian <tylerkandarian@Tylers-MacBook-Pro.local>
2022-10-03 17:58:53 -0400

    working js

--- Develop/script.js
+++ Develop/script.js
@@ -16,39 +16,84 @@ var hour = moment().hours();
 var userInput;
 var hourSpan;
 
-var interval = setInterval(function() {
+var interval = setInterval(function () {
     var momentNow = moment();
     $('#currentDay').html(momentNow.format('YYYY MMMM DD') + ' '
-                        + momentNow.format('dddd')
-                         .substring(0,3).toUpperCase());
+        + momentNow.format('dddd')
+            .substring(0, 3).toUpperCase());
     $('#currentDay').html(currentDate + " " + momentNow.format('hh:mm:ss A'));
-  }, 100);
+}, 100);
 
 function initPage() {
-  var init = JSON.parse(localStorage.getItem("9 am"));
-  nineAm.text(init);
-  console.log(hour);
 
-  if (hour > hourSched) {
-    console.log("yeah");
-    nineAm.addClass("past");
-  } else {
-    console.log("nooo");
-  }
+    console.log("Current Hour " + hour);
+    var init9 = JSON.parse(localStorage.getItem("09:00 am"));
+    nineAm.val(init9);
 
+    var init10 = JSON.parse(localStorage.getItem("10:00 am"))
+    tenAm.val(init10);
 
-} 
+    var init11 = JSON.parse(localStorage.getItem("11:00 am"))
+    elevenAm.val(init11);
 
-$(document).ready(function(){
-  initPage()
+    var init12 = JSON.parse(localStorage.getItem("12:00 pm"))
+    twelvePm.val(init12);
 
-    $("#button9am").on("click", function() {
-        var input = $.trim(nineAm.val());
-        console.log("9 am " + input);
-        localStorage.setItem("9 am", JSON.stringify(input));
-        localInput = JSON.parse(localStorage.getItem("9 am"));
-        console.log(localInput);
+    var init1 = JSON.parse(localStorage.getItem("01:00 pm"))
+    onePm.val(init1);
 
-      });
+    var init2 = JSON.parse(localStorage.getItem("02:00 pm"))
+    twoPm.val(init2);
+
+    var init3 = JSON.parse(localStorage.getItem("03:00 pm"))
+    threePm.val(init3);
+
+    var init4 = JSON.parse(localStorage.getItem("04:00 pm"))
+    fourPm.val(init4);
+
+    var init5 = JSON.parse(localStorage.getItem("05:00 pm"))
+    fivePm.val(init5);
+
+    var init6 = JSON.parse(localStorage.getItem("06:00 pm"))
+    sixPm.val(init6);
+
+    var init7 = JSON.parse(localStorage.getItem("07:00 pm"))
+    sevenPm.val(init7);
+}
+
+function background() {
+
+    $(".form-control").each(function () {
+        var timeTest = parseInt($(this).attr("id"));
+        hour = parseInt(hour);
+        console.log(timeTest);
+        console.log(hour);
+        if (hour > timeTest) {
+            $(this).addClass("past");
+        } else if (hour < timeTest) {
+            $(this).addClass("future");
+        } else {
+            $(this).addClass("present");
+        }
+    });
+}
+
+$(document).ready(function () {
+    initPage()
+    background()
+
+    $(".saveBtn").on("click", function () {
+        userInput = $(this).siblings(".form-control").val().trim();
+        console.log(userInput);
+        hourSpan = $(this).siblings(".input-group-prepend").text().trim();
+        console.log(hourSpan);
+        localStorage.setItem(hourSpan, JSON.stringify(userInput));
+
+    })
+    // Button for clear the day
+    $("#clearDay").on("click", function () {
+        localStorage.clear();
+        initPage()
+    })
 
 });
