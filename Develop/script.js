12dab6952acd6ba8574c6f681a463fb20bb9d9b0
Tyler Kandarian <tylerkandarian@Tylers-MacBook-Pro.local>
2022-10-03 17:10:24 -0400

    added working timer and date

--- Develop/script.js
+++ Develop/script.js
@@ -1,14 +1,28 @@
 var currentDate = moment().format('dddd') + " " + moment().format("Do MMM YYYY");
 var currentHour = moment().format('h:mm:ss a');
-var nineAm = $("#text9am");
-var span9am = $("#9am");
-var tenAm = $("#text10am");
-var hour = moment().format('h a');
-var hourSched = $(span9am).text();
+var nineAm = $("#9am");
+var tenAm = $("#10am");
+var elevenAm = $("#11am");
+var twelvePm = $("#12pm");
+var onePm = $("#13pm");
+var twoPm = $("#14pm");
+var threePm = $("#15pm");
+var fourPm = $("#16pm");
+var fivePm = $("#17pm");
+var sixPm = $("#18pm");
+var sevenPm = $("#19pm");
 
-// var userInput = ""
+var hour = moment().hours();
+var userInput;
+var hourSpan;
 
-$("#currentDay").text(currentDate + " " + currentHour);
+var interval = setInterval(function() {
+    var momentNow = moment();
+    $('#currentDay').html(momentNow.format('YYYY MMMM DD') + ' '
+                        + momentNow.format('dddd')
+                         .substring(0,3).toUpperCase());
+    $('#currentDay').html(currentDate + " " + momentNow.format('hh:mm:ss A'));
+  }, 100);
 
 function initPage() {
   var init = JSON.parse(localStorage.getItem("9 am"));
